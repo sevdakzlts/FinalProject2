45ba4b4ed643138233659635050792cff5518404
murat.inal1626@gmail.com <murat.inal1626@gmail.com>
2021-03-10 20:08:45 +0300

    Fix GetAll Function

--- Fatih.Service/Service/ProductService.cs
+++ Fatih.Service/Service/ProductService.cs
@@ -1,19 +1,32 @@
 ﻿using Fatih.Core.Models;
+using Fatih.Data.Context;
 using FinalProject.Core.Repositories;
 using FinalProject.Core.Services;
 using FinalProject.Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Fatih.Service.Service
 {
     public class ProductService : Service<Product>, IProductService
     {
-        public ProductService(IUnitOfWork unitOfWork,IRepository<Product> repository):base(unitOfWork,repository)
+        private readonly AppDbContext _dbContext;
+        public ProductService(IUnitOfWork unitOfWork, IRepository<Product> repository, AppDbContext dbContext) : base(unitOfWork, repository)
         {
-
+            _dbContext = dbContext;
         }
         public void DeleteById(int productId)
         {
-              _unitOfWork.Products.DeleteById(productId);
+            _unitOfWork.Products.DeleteById(productId);
+        }
+
+        public async Task<ICollection<Product>> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            return await _dbContext.Products.Include(prop => prop.Category).Include(p=>p.ProductComments).ToListAsync();
         }
     }
 }
